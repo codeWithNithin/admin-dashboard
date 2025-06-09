e815ee6ebf6b5dbbf3f99c31883f95bb92d6e033
codeWithNithin <nithinkottary25@gmail.com>
2025-06-09 12:58:22 +0530

    add auto refresh token interceptors

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,12 +1,47 @@
-import axios  from "axios";
+import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_API_URL,
-    // this is for storing cookie
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-        Accept: 'application/json'
-    },
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  // this is for storing cookie
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+    Accept: "application/json",
+  },
+});
 
-})
+const refreshToken = async () => {
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+};
+
+// use interceptors on response for exchanging access token and refresh token when access token expires
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    // isRetry flag is for handling infinite loop in interceptors
+    if (error.response.status === 401 && !originalRequest._isRetry) {
+      try {
+        originalRequest._isRetry = true;
+        const headers = { ...originalRequest.headers };
+        // fetch new access and refresh token
+        await refreshToken();
+        //  pass the entire request and headers to the previosly called api
+        return api.request({ ...originalRequest, headers });
+      } catch (err) {
+        console.log(err);
+        useAuthStore.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,6 +3,7 @@ import { self } from "../http/api";
 import { useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "../store";
 import { useEffect } from "react";
+import { AxiosError } from "axios";
 
 const getSelf = async () => {
   const { data } = await self();
@@ -14,10 +15,18 @@ const Root = () => {
   const { data, isLoading } = useQuery({
     queryKey: ["self"],
     queryFn: getSelf,
+    retry: (failureCount, err) => {
+      // dont call self endpoint if status code is 401, else call 3 times and stop.
+      if (err instanceof AxiosError && err.response?.status === 401) {
+        return false;
+      }
+
+      return failureCount < 3;
+    },
   });
 
   useEffect(() => {
-    console.log('data:', data)
+    console.log("data:", data);
     if (data) {
       setUser(data);
     }
