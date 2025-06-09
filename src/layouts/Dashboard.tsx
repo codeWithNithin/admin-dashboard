dc791aa103fe17791b2075a8490a917b1ee585b3
codeWithNithin <nithinkottary25@gmail.com>
2025-06-09 15:15:43 +0530

    add commit layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,15 +1,92 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import { Layout, Menu, theme } from "antd";
+import Icon from "@ant-design/icons";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import { foodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+import UserIcon from "../components/icons/UserIcon";
+
+const { Sider, Header, Content, Footer } = Layout;
+
+const baseItems = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+
+  {
+    key: "/products",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/orders",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/orders">Orders</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+  {
+    key: "/restaurants",
+    icon: <Icon component={foodIcon} />,
+    label: <NavLink to="/restaurants">Restaurants</NavLink>,
+  },
+  {
+    key: "/users",
+    icon: <Icon component={UserIcon} />,
+    label: <NavLink to="/users">Users</NavLink>,
+  },
+];
 
 const Dashboard = () => {
-  const {user} = useAuthStore();
+  const [collapsed, setCollapsed] = useState(false);
 
-  if(user === null) return <Navigate to='/auth/login' replace={true} />
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const { user } = useAuthStore();
+
+  if (user === null) return <Navigate to="/auth/login" replace={true} />;
 
   return (
     <div>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div className="demo-logo-vertical logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            items={baseItems}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
       <h1> Dashboard </h1>
-      <Outlet />
     </div>
   );
 };
