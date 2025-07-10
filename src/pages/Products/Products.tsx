aef4dcf89911151fed4d97e4f77b8ee88514c2ae
codeWithNithin <nithinkottary25@gmail.com>
2025-07-10 15:14:17 +0530

    add  breadcrums to products page

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,8 +1,23 @@
+import { Breadcrumb, Space } from "antd";
+import { Link } from "react-router-dom";
+import { RightOutlined } from "@ant-design/icons";
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <Space direction="vertical" size="large" style={{ width: "100%" }}>
+      <Breadcrumb
+        separator={<RightOutlined />}
+        items={[
+          {
+            title: <Link to="/">Dashboard</Link>,
+          },
+          {
+            title: "Products",
+          },
+        ]}
+      />
+    </Space>
+  );
+};
 
-export default Products
+export default Products;
